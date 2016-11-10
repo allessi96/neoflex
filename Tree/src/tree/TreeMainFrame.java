c647caa43b131b3b053b47a567a1e716cefdd7d1
allessi96 <kattner@bk.ru>
2016-11-11 00:59:08 +0300

    Рекурсия (поиск)

--- Tree/src/tree/TreeMainFrame.java
+++ Tree/src/tree/TreeMainFrame.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class TreeMainFrame extends javax.swing.JFrame {
@@ -12,6 +13,7 @@ public class TreeMainFrame extends javax.swing.JFrame {
     public static ArrayList<Node> nodes = new ArrayList<>();
     Graphics g;
     Graphics2D gr2d;
+    Node root;
     Node selectedNode;
 
     public TreeMainFrame() {
@@ -25,7 +27,7 @@ public class TreeMainFrame extends javax.swing.JFrame {
         this.g = DrawPanel.getGraphics();
         this.gr2d = (Graphics2D) g;
         gr2d.setBackground(Color.white);
-        gr2d.setColor(Color.green);
+        gr2d.setColor(Color.black);
     }
 
     @SuppressWarnings("unchecked")
@@ -48,7 +50,11 @@ public class TreeMainFrame extends javax.swing.JFrame {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 formKeyPressed(evt);
             }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                formKeyReleased(evt);
+            }
         });
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         DrawPanel.setBackground(new java.awt.Color(255, 255, 255));
         DrawPanel.setPreferredSize(new java.awt.Dimension(440, 450));
@@ -67,12 +73,15 @@ public class TreeMainFrame extends javax.swing.JFrame {
             .addGap(0, 0, Short.MAX_VALUE)
         );
 
+        getContentPane().add(DrawPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 11, -1, 427));
+
         newNode.setText("Создать узел");
         newNode.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 newNodeActionPerformed(evt);
             }
         });
+        getContentPane().add(newNode, new org.netbeans.lib.awtextra.AbsoluteConstraints(468, 11, 222, 39));
 
         removeNode.setText("Удалить узел");
         removeNode.addActionListener(new java.awt.event.ActionListener() {
@@ -80,6 +89,7 @@ public class TreeMainFrame extends javax.swing.JFrame {
                 removeNodeActionPerformed(evt);
             }
         });
+        getContentPane().add(removeNode, new org.netbeans.lib.awtextra.AbsoluteConstraints(468, 100, 222, 39));
 
         searchName.setText("Поиск по имени");
         searchName.addActionListener(new java.awt.event.ActionListener() {
@@ -87,6 +97,7 @@ public class TreeMainFrame extends javax.swing.JFrame {
                 searchNameActionPerformed(evt);
             }
         });
+        getContentPane().add(searchName, new org.netbeans.lib.awtextra.AbsoluteConstraints(468, 189, 222, 39));
 
         searchValue.setText("Поиск по значению");
         searchValue.addActionListener(new java.awt.event.ActionListener() {
@@ -94,50 +105,13 @@ public class TreeMainFrame extends javax.swing.JFrame {
                 searchValueActionPerformed(evt);
             }
         });
+        getContentPane().add(searchValue, new org.netbeans.lib.awtextra.AbsoluteConstraints(468, 278, 222, 39));
 
         allValue.setText("Вывод всех значений");
+        getContentPane().add(allValue, new org.netbeans.lib.awtextra.AbsoluteConstraints(468, 367, 222, 39));
 
         Dev.setText("Пархоменко Александра");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(DrawPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(newNode, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(removeNode, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(searchName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(searchValue, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(allValue, javax.swing.GroupLayout.DEFAULT_SIZE, 222, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(Dev)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(newNode, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(50, 50, 50)
-                        .addComponent(removeNode, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(49, 49, 49)
-                        .addComponent(searchName, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
-                        .addComponent(searchValue, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(50, 50, 50)
-                        .addComponent(allValue, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(Dev))
-                    .addComponent(DrawPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 428, Short.MAX_VALUE))
-                .addContainerGap())
-        );
+        getContentPane().add(Dev, new org.netbeans.lib.awtextra.AbsoluteConstraints(565, 424, -1, -1));
 
         getAccessibleContext().setAccessibleDescription("");
         getAccessibleContext().setAccessibleParent(this);
@@ -146,30 +120,6 @@ public class TreeMainFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-//обработчик нажатий на стрелки
-    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
-
-        System.out.println(this.isFocusable());
-        if (!nodes.isEmpty()) {
-            if (KeyEvent.getKeyText(evt.getKeyCode()).equals("Up") && (selectedNode.getParent() != null)) {
-                System.out.println("up");
-                selectedNode = selectedNode.getParent();
-            } else if (KeyEvent.getKeyText(evt.getKeyCode()).equals("Left") && (selectedNode.getLeft() != null)) {
-                System.out.println("left");
-                selectedNode = selectedNode.getLeft();
-            } else if (KeyEvent.getKeyText(evt.getKeyCode()).equals("Right") && (selectedNode.getRight() != null)) {
-                System.out.println("rig");
-                selectedNode = selectedNode.getRight();
-            }
-        }
-
-        System.out.println("SN=" + selectedNode.name);
-
-        gr2d.clearRect(0, 0, 440, 450);
-        drawNode(Math.abs(DrawPanel.getWidth() / 2 - 10), 10, nodes.get(0));
-
-    }//GEN-LAST:event_formKeyPressed
 
     private void newNodeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newNodeActionPerformed
         new AddNewNode().setMainFrame(TreeMainFrame.this);
@@ -205,42 +155,79 @@ public class TreeMainFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_removeNodeActionPerformed
 
     private void searchNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchNameActionPerformed
-
-        SearchNode sn = new SearchNode();
-        sn.setMainFrame(TreeMainFrame.this);
-        sn.nameText.setEnabled(true);
-        setEnabled(false);
-        this.requestFocus();
-
+        if (!nodes.isEmpty()) {
+            SearchNode sn = new SearchNode();
+            sn.setMainFrame(TreeMainFrame.this);
+            sn.nameText.setEnabled(true);
+            setEnabled(false);
+            this.requestFocus();
+        }
     }//GEN-LAST:event_searchNameActionPerformed
 
     private void searchValueActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchValueActionPerformed
-
-        SearchNode sn = new SearchNode();
-        sn.setMainFrame(TreeMainFrame.this);
-        sn.valText.setEnabled(true);
-        setEnabled(false);
-        this.requestFocus();
-
+        if (!nodes.isEmpty()) {
+            SearchNode sn = new SearchNode();
+            sn.setMainFrame(TreeMainFrame.this);
+            sn.valText.setEnabled(true);
+            setEnabled(false);
+            this.requestFocus();
+        }
     }//GEN-LAST:event_searchValueActionPerformed
 
-    public void drawNode(int x, int y, Node n) {
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formKeyPressed
 
-        if (n.equals(selectedNode)) {
-            gr2d.fillOval(x, y, 20, 20);
-        } else {
-            gr2d.drawOval(x, y, 20, 20);
+    private void formKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyReleased
+        // TODO add your handling code here:
+
+        System.out.println(this.isFocusable());
+        if (!nodes.isEmpty()) {
+            if (KeyEvent.getKeyText(evt.getKeyCode()).equals("Up") && (selectedNode.getParent() != null)) {
+                System.out.println("up");
+                selectedNode = selectedNode.getParent();
+            } else if (KeyEvent.getKeyText(evt.getKeyCode()).equals("Left") && (selectedNode.getLeft() != null)) {
+                System.out.println("left");
+                selectedNode = selectedNode.getLeft();
+            } else if (KeyEvent.getKeyText(evt.getKeyCode()).equals("Right") && (selectedNode.getRight() != null)) {
+                System.out.println("rig");
+                selectedNode = selectedNode.getRight();
+            }
         }
+
+        System.out.println("SN=" + selectedNode.name);
+
+        gr2d.clearRect(0, 0, 440, 450);
+        drawNode(Math.abs(DrawPanel.getWidth() / 2 - 10), 10, nodes.get(0));
+
+    }//GEN-LAST:event_formKeyReleased
+
+    public void drawNode(int x, int y, Node n) {
 
         Node l = n.getLeft();
         if (l != null) {
             drawNode(x - 50, y + 30, n.getLeft());
+            gr2d.drawLine(x + 10, y + 20, x - 40, y + 30);
+
         }
 
         Node r = n.getRight();
         if (r != null) {
             drawNode(x + 50, y + 30, n.getRight());
+            gr2d.drawLine(x + 10, y + 20, x + 60, y + 30);
         }
+
+        if (n.equals(selectedNode)) {
+            gr2d.setColor(Color.red);
+        }
+        gr2d.fillOval(x, y, 20, 20);
+
+        gr2d.setColor(Color.black);
+
+        JLabel valueNodeLabel = new JLabel(Integer.toString(n.getValue()));
+        valueNodeLabel.setAlignmentX(x - 20);
+        valueNodeLabel.setAlignmentY(y);
+        DrawPanel.add(valueNodeLabel);
 
     }
 
